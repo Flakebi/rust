fd5c487cc092ae45bb4c3df032c94616b197821f
Flakebi <flakebi@t-online.de>
2025-01-21 23:21:29 +0100

    Add amdgpu to gpu-kernel test
    Check that the gpu-kernel calling convention translates to
    `amdgpu_kernel` when compiling for the amdgpu target.

--- tests/codegen/gpu-kernel-abi.rs
+++ tests/codegen/gpu-kernel-abi.rs
@@ -1,6 +1,8 @@
 // Checks that the gpu-kernel calling convention correctly translates to LLVM calling conventions.
 
-//@ revisions: nvptx
+//@ revisions: amdgpu nvptx
+//@ [amdgpu] compile-flags: --crate-type=rlib --target=amdgcn-amd-amdhsa -Ctarget-cpu=gfx900
+//@ [amdgpu] needs-llvm-components: amdgpu
 //@ [nvptx] compile-flags: --crate-type=rlib --target=nvptx64-nvidia-cuda
 //@ [nvptx] needs-llvm-components: nvptx
 #![feature(no_core, lang_items, abi_gpu_kernel)]
@@ -13,6 +15,7 @@ trait Freeze {}
 #[lang = "copy"]
 trait Copy {}
 
+// amdgpu: define amdgpu_kernel void @fun(i32
 // nvptx: define ptx_kernel void @fun(i32
 #[no_mangle]
 pub extern "gpu-kernel" fn fun(_: i32) {}
