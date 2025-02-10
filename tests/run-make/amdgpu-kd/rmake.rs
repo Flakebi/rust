9cf3f01d47ee7b3c1935f71dfa447272a0aad4aa
Flakebi <flakebi@t-online.de>
2025-02-10 21:25:55 +0100

    Export kernel descriptor for amdgpu kernels
    The host runtime (HIP or HSA) expects a kernel descriptor object for
    each kernel in the ELF file. The amdgpu LLVM backend generates the
    object. It is created as a symbol with the name of the kernel plus a
    `.kd` suffix.
    
    Add it to the exported symbols in the linker script, so that it can be
    found.

---
+++ tests/run-make/amdgpu-kd/rmake.rs
@@ -0,0 +1,16 @@
+// Checks that the produced object has the .kd symbol exported.
+
+//@ needs-rust-lld
+
+use run_make_support::{cmd, dynamic_lib_name, llvm_readobj, run_in_tmpdir, rustc};
+
+fn main() {
+    rustc()
+        .crate_name("foo")
+        .target("amdgcn-amd-amdhsa")
+        .arg("-Ctarget-cpu=gfx900")
+        .crate_type("cdylib")
+        .input("foo.rs")
+        .run();
+    llvm_readobj().input("foo.elf").symbols().run().assert_stdout_contains("kernel.kd");
+}
